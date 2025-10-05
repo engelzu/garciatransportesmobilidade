4a9f0b343515a2ba2b77bdee1fa63785722cd380
engelzu <engelmobile2020@gmail.com>
2025-10-05 11:51:16 -0300

    Update stripe-payment.js

--- api/stripe-payment.js
+++ api/stripe-payment.js
@@ -1,6 +1,6 @@
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
@@ -61,4 +61,4 @@ export default async function handler(req, res) {
             message: error.message
         });
     }
-}
+};
