45028e69eea130461ceab1f6afca66ae1a162912
engelzu <engelmobile2020@gmail.com>
2025-10-05 22:48:23 -0300

    Delete api/webhook directory

--- api/webhook/index.js
+++
@@ -1,5 +0,0 @@
-// Importa a sua função original que já existe
-const webhookHandler = require('../webhook.js');
-
-// Exporta a função para ser usada nesta nova rota
-module.exports = webhookHandler;
